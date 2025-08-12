f52a9befc0ade0c9ccf6d5a837a7d36fde86c34a
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2025-08-12 09:32:01 -0300

    Atualização

--- src/main/java/strategy/FretePAC.java
+++ src/main/java/strategy/FretePAC.java
@@ -3,6 +3,6 @@ package main.java.strategy;
 public class FretePAC implements CalculoFrete {
     @Override
     public double calcular(double pesoEmKg) {
-        return pesoEmKg * 8.00; //nesse caso, 8 reais por kg
+        return pesoEmKg * 8.00; 
     }
 }
